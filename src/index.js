f345ba908d90e3889d156ce86992ab88fcbd1f55
aducksaidmeow <Duck7D6@gmail.com>
2022-11-02 22:11:19 +0700

    add GoogleOAuthProvider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Login from './components/login'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+    <Login />
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

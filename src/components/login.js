afea2aca992ac7502bc0724b48ef3b134dac0151
aducksaidmeow <Duck7D6@gmail.com>
2022-11-03 20:46:28 +0700

    add axios call

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,6 @@
 import { useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 export default function Login() {
 
@@ -6,6 +8,12 @@ export default function Login() {
         flow: 'auth-code',
         onSuccess: codeResponse => {
             console.log(codeResponse);
+            const url = process.env.NODE_ENV === 'production' ? 
+                `${process.env.REACT_APP_API_URL}/api/test` :
+                '/api/test';
+            axios.post(url, {}).then(response => {
+                console.log(response.data);
+            }).catch(error => console.log(error.message));
         }
     });
 

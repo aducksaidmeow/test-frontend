b0f2e6bdbcc8f64cc1e59b29ea9ef2af0406d6ce
aducksaidmeow <Duck7D6@gmail.com>
2022-11-07 19:23:42 +0700

    fix typo

--- src/components/student/studentCalendar.js
+++ src/components/student/studentCalendar.js
@@ -5,7 +5,7 @@ export default class StudentCalendar extends React.Component {
   render() {
     return (
       <div>
-        <Calendar apiKey={process.env.REACT_APP_API_KEY} calendars={[ { calendarId: localStorage.getItem('fullEmail') } ]} />
+        <Calendar apiKey={process.env.REACT_APP_API_KEY} calendars={[ { calendarId: localStorage.getItem('email') } ]} />
       </div>
     );
   }

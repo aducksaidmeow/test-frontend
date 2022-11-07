02787272dfcd7090047babe32ac80e49e5007fe5
aducksaidmeow <Duck7D6@gmail.com>
2022-11-07 20:23:11 +0700

    add navigation

--- src/components/addInfo.js
+++ src/components/addInfo.js
@@ -1,7 +1,10 @@
 import { useForm } from 'react-hook-form'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddInfo() {
+
+    const navigate = useNavigate();
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
@@ -13,6 +16,8 @@ export default function AddInfo() {
         const url = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_API_URL}/api/add-role` : '/api/add-role';
         axios.post(url, { userId, role }).then(response => {
             console.log(response.data);
+            if (role === 'student') navigate('/student-calendar');
+            else if (role === 'teacher') navigate('/teacher-calendar');
         }).catch(error => console.log(error.message));
     };
 

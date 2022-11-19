f6c8b7840d331364abaa2954f32ab41f661ccb57
aducksaidmeow <Duck7D6@gmail.com>
2022-11-19 22:13:07 +0700

    add teacher prefix

--- src/components/teacher/calendar/displayEvent.js
+++ src/components/teacher/calendar/displayEvent.js
@@ -5,12 +5,12 @@ import './displayEvent.css'
 export default function DisplayEvent({ open, setOpen, content, setContent }) {
 
     return(
-        <div className="display-event-container">
-            <div className="display-event-close-button" onClick={() => setOpen(false)}>x</div>
-            <div className="display-event-title">{content.title}</div>
-            <div className="display-event-description">{content.description}</div>
-            <div className="display-event-group">{content.group}</div>
-            <div className="display-event-start-time">{formatDate(content.startTime, {
+        <div className="teacher-display-event-container">
+            <div className="teacher-display-event-close-button" onClick={() => setOpen(false)}>x</div>
+            <div className="teacher-display-event-title">{content.title}</div>
+            <div className="teacher-display-event-description">{content.description}</div>
+            <div className="teacher-display-event-group">{content.group}</div>
+            <div className="teacher-display-event-start-time">{formatDate(content.startTime, {
                 year: "numeric",
                 month: "2-digit",
                 day: "2-digit",
@@ -20,7 +20,7 @@ export default function DisplayEvent({ open, setOpen, content, setContent }) {
                 second: "2-digit",
                 hour12: true
             })}</div>
-            <div className="display-event-end-time">{content.endTime === null ? formatDate(content.startTime, {
+            <div className="teacher-display-event-end-time">{content.endTime === null ? formatDate(content.startTime, {
                 year: "numeric",
                 month: "2-digit",
                 day: "2-digit",
@@ -39,7 +39,7 @@ export default function DisplayEvent({ open, setOpen, content, setContent }) {
                 second: "2-digit",
                 hour12: true
             })}</div>
-            <div className="display-event-id">{content.id}</div>
+            <div className="teacher-display-event-id">{content.id}</div>
         </div>
     );
 }

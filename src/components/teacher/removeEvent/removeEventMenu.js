79bd8ff3168c1974e7a74c35d1838de067d26922
aducksaidmeow <Duck7D6@gmail.com>
2022-11-24 18:35:57 +0700

    reformat code and add animated loading screen

--- src/components/teacher/removeEvent/removeEventMenu.js
+++ src/components/teacher/removeEvent/removeEventMenu.js
@@ -1,31 +1,56 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './removeEventMenu.css'
+import "./removeEventMenu.css";
+import LoadingScreen from "./loadingScreen/loadingScreen";
 
 export default function RemoveEventMenu({ open, setOpen }) {
+  const [eventId, setEventId] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const [eventId, setEventId] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const url =
+      process.env.NODE_ENV === "production"
+        ? `${process.env.REACT_APP_API_URL}/api/remove-event`
+        : "/api/remove-event";
+    const userId = localStorage.getItem("userId");
+    axios
+      .post(url, { userId, eventId })
+      .then((response) => {
+        console.log(response.data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error.message));
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const url = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_API_URL}/api/remove-event` : '/api/remove-event';
-        const userId = localStorage.getItem('userId');
-        axios.post(url, { userId, eventId}).then(response => {
-            console.log(response.data);
-        }).catch(error => console.log(error.message));
-    }
+  const onChange = (e) => {
+    setEventId(e.target.value);
+  };
 
-    const onChange = (e) => {
-        setEventId(e.target.value);
-    }
-
-    return (
-        <div className="remove-event-form-container">
-            <div className="remove-event-close-button" onClick={() => setOpen(false)}>x</div>
-            <form onSubmit={(e) => onSubmit(e)}>
-                <input className="event-id" type="text" placeholder="Enter event ID" required onChange={(e) => onChange(e)}/>
-                <input className="remove-event-submit-button" type="submit" />
-            </form>
+  return (
+    <div className="remove-event-form-container">
+      {!loading && (
+        <div
+          className="remove-event-close-button"
+          onClick={() => setOpen(false)}
+        >
+          x
         </div>
-    );
+      )}
+      {!loading && (
+        <form onSubmit={(e) => onSubmit(e)}>
+          <input
+            className="event-id"
+            type="text"
+            placeholder="Enter event ID"
+            required
+            onChange={(e) => onChange(e)}
+          />
+          <input className="remove-event-submit-button" type="submit" />
+        </form>
+      )}
+      {loading && <LoadingScreen />}
+    </div>
+  );
 }

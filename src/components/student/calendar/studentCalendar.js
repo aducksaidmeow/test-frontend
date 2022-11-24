993824ed6b054166f67a58c2ad75da09725af61c
aducksaidmeow <Duck7D6@gmail.com>
2022-11-24 18:31:54 +0700

    reformat code and add animated loading screen

--- src/components/student/calendar/studentCalendar.js
+++ src/components/student/calendar/studentCalendar.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import googleCalendarPlugin from '@fullcalendar/google-calendar'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
 import DisplayEvent from "./displayEvent";
 import LoadingScreen from "./loadingScreen/loadingScreen";
-import axios from 'axios'
-import './studentCalendar.css'
+import axios from "axios";
+import "./studentCalendar.css";
 
 export default function StudentCalendar() {
-
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [content, setContent] = useState({});
@@ -17,44 +16,59 @@ export default function StudentCalendar() {
     eventInfo.jsEvent.preventDefault();
     console.log(eventInfo);
     setLoading(true);
-    const eid = eventInfo.event.url.split('=')[1];
-    const userId = localStorage.getItem('userId');
-    const url = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_API_URL}/api/get-event` : '/api/get-event';
-    axios.post(url, { userId, eid }).then(response => {
-      console.log(response.data);
-      const eventContent = {
-        title: eventInfo.event.title,
-        description: response.data.data.description,
-        group: response.data.data.extendedProperties.shared.groupName,
-        startTime: eventInfo.event.start,
-        endTime: eventInfo.event.end,
-        id: response.data.data.id
-      }
-      setContent(eventContent);
-      setLoading(false);
-      setOpen(true);
-      console.log(content);
-    }).catch(error => console.log(error));  
-  }
+    const eid = eventInfo.event.url.split("=")[1];
+    const userId = localStorage.getItem("userId");
+    const url =
+      process.env.NODE_ENV === "production"
+        ? `${process.env.REACT_APP_API_URL}/api/get-event`
+        : "/api/get-event";
+    axios
+      .post(url, { userId, eid })
+      .then((response) => {
+        console.log(response.data);
+        const eventContent = {
+          title: eventInfo.event.title,
+          description: response.data.data.description,
+          group: response.data.data.extendedProperties.shared.groupName,
+          startTime: eventInfo.event.start,
+          endTime: eventInfo.event.end,
+          id: response.data.data.id,
+        };
+        setContent(eventContent);
+        setLoading(false);
+        setOpen(true);
+        console.log(content);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div>
       <div className="student-calendar-container">
-        { !loading && <FullCalendar 
-          plugins={[dayGridPlugin, googleCalendarPlugin]}
-          initialView="dayGridMonth"
-          googleCalendarApiKey={process.env.REACT_APP_API_KEY}
-          events={{
-            googleCalendarId: localStorage.getItem('email')
-          }}
-          contentHeight="auto"
-          eventDisplay="block"
-          eventColor="#87CEEB"
-          eventClick={(eventInfo) => eventClick(eventInfo)}
-        /> }
+        {!loading && (
+          <FullCalendar
+            plugins={[dayGridPlugin, googleCalendarPlugin]}
+            initialView="dayGridMonth"
+            googleCalendarApiKey={process.env.REACT_APP_API_KEY}
+            events={{
+              googleCalendarId: localStorage.getItem("email"),
+            }}
+            contentHeight="auto"
+            eventDisplay="block"
+            eventColor="#87CEEB"
+            eventClick={(eventInfo) => eventClick(eventInfo)}
+          />
+        )}
       </div>
-      { open && <DisplayEvent open={open} setOpen={setOpen} content={content} setContent={setContent} /> }
-      { loading && <LoadingScreen /> }
+      {open && (
+        <DisplayEvent
+          open={open}
+          setOpen={setOpen}
+          content={content}
+          setContent={setContent}
+        />
+      )}
+      {loading && <LoadingScreen />}
     </div>
   );
 }

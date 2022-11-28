aef1b377b6a4fd45a0d528495fc9ff2e3d3b5e29
aducksaidmeow <Duck7D6@gmail.com>
2022-11-28 11:29:30 +0700

    add tailwindcss and remake teacher-calendar page

--- src/components/teacher/teacherMain.js
+++ src/components/teacher/teacherMain.js
@@ -1,27 +1,56 @@
+import React, { useState } from "react";
 import TeacherCalendar from "./calendar/teacherCalendar";
 import AddEventButton from "./addEvent/addEventButton";
 import AddGroupButton from "./addGroup/addGroupButton";
 import RemoveEventButton from "./removeEvent/removeEventButton";
+import DisplayEvent from "./calendar/displayEvent"
+import AddEventMenu from "./addEvent/addEventMenu";
+import AddGroupMenu from "./addGroup/addGroupMenu"
+import RemoveEventMenu from "./removeEvent/removeEventMenu";
 
 export default function TeacherMain() {
+
+    const [render, setRender] = useState({
+        loading: false,
+        calendar: true,
+        display: false,
+        addEvent: false,
+        addGroup: false,
+        removeEvent: false,
+    })
+
+    const [display, setDisplay] = useState({});
 
     const email = localStorage.getItem('email');
     const role = localStorage.getItem('role');
 
-    if (email != '' && email != null && role === 'teacher') {
+    if (email !== '' && email != null && role === 'teacher') {
         return (
-            <div>
-                <AddGroupButton />
-                <AddEventButton />
-                <RemoveEventButton />
-                <TeacherCalendar />
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <div>Something is wrong!</div>
+            <div className="h-screen grid grid-cols-16 bg-[#F0EBE3]">
+                <div className="flex flex-col items-center pt-[10vh] gap-[1vh]">
+                    <AddGroupButton render={render} setRender={setRender} />
+                    <AddEventButton render={render} setRender={setRender} />
+                    <RemoveEventButton render={render} setRender={setRender}/>
+                </div>
+                <div className="col-start-2 col-span-15 flex justify-center items-center">
+                    {render.calendar && 
+                        <TeacherCalendar render={render} setRender={setRender} display={display} setDisplay={setDisplay}/>
+                    }
+                    {render.display && 
+                        <DisplayEvent render={render} setRender={setRender} display={display} setDisplay={setDisplay}/>
+                    }
+                    {render.addEvent &&
+                        <AddEventMenu render={render} setRender={setRender} />
+                    }
+                    {render.addGroup && 
+                        <AddGroupMenu render={render} setRender={setRender} />
+                    }
+                    {render.removeEvent &&
+                        <RemoveEventMenu render={render} setRender={setRender} />
+                    }
+                </div>
             </div>
         );
     }
+
 }

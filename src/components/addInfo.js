1faff00e317be59a85c12e66c61a75f9642cd43f
aducksaidmeow <Duck7D6@gmail.com>
2022-11-28 11:28:34 +0700

    add tailwindcss and redesign add-info page

--- src/components/addInfo.js
+++ src/components/addInfo.js
@@ -1,52 +1,64 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./addInfo.css";
-import AddInfoLoadingScreen from "./loadingScreen/addInfoLoadingScreen";
+import student from "./student.jpg";
+import teacher from "./teacher.jpg";
 
 export default function AddInfo() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
   const onClick = (role) => {
-    console.log(role);
     localStorage.setItem("role", role);
     setLoading(true);
     const userId = localStorage.getItem("userId");
-    const url =
-      process.env.NODE_ENV === "production"
-        ? `${process.env.REACT_APP_API_URL}/api/add-role`
-        : "/api/add-role";
-    axios
-      .post(url, { userId, role })
-      .then((response) => {
-        console.log(response.data);
-        setLoading(false);
-        if (role === "student") navigate("/student-calendar");
-        else if (role === "teacher") navigate("/teacher-calendar");
-      })
-      .catch((error) => console.log(error.message));
+    const url = process.env.NODE_ENV === "production" ? `${process.env.REACT_APP_API_URL}/api/add-role` : "/api/add-role";
+    axios.post(url, { userId, role }).then((response) => {
+      setLoading(false);
+      if (role === "student") navigate("/student-calendar");
+      else if (role === "teacher") navigate("/teacher-calendar");
+    }).catch((error) => console.log(error.message));
   };
+
+  const [index, setIndex] = useState(0);
+  const loadingDisplay = ["Loading.", "Loading..", "Loading..."];
+
+  useEffect(() => {
+    const currentInterval = setInterval(() => setIndex((index) => (index + 1) % 3), 1000);
+    return () => clearInterval(currentInterval);
+  }, []);
 
   const email = localStorage.getItem("email");
   const role = localStorage.getItem("role");
 
   if (loading) {
-    return <AddInfoLoadingScreen />;
+    return  (
+      <div className="h-screen flex justify-center items-center bg-[#F0EBE3]">
+        <div className="h-[80vh] w-[60vw] bg-[#E4DCCF] shadow-2xl grid grid-rows-1 grid-cols-2">
+          <div className="flex justify-center items-center row-span-2 col-span-3 font-['consolas'] text-[40px]">
+            {loadingDisplay[index]}
+          </div>
+        </div>  
+      </div>
+    );
   } else if (email !== "" && email != null && role === "") {
     return (
-      <div className="add-info-background">
-        {!loading && (
-          <div className="add-info-container">
-            <h1 className="role-title">Choose a role to continue</h1>
-            <button className="student" onClick={() => onClick("student")}>
-              Student
-            </button>
-            <button className="teacher" onClick={() => onClick("teacher")}>
-              Teacher
-            </button>
-          </div>
-        )}
+      <div className="h-screen flex justify-center items-center bg-[#F0EBE3]">
+        <div className="h-[80vh] w-[60vw] bg-[#E4DCCF] shadow-2xl grid grid-rows-1 grid-cols-2">
+          {!loading && (
+          <>
+            <div className="flex justify-center items-center">
+              <button className="bg-[#7D9D9C] h-[10vh] w-[20vw] font-['consolas'] text-[30px] rounded-md hover:scale-[1.05]" onClick={() => onClick("student")}>
+                Student
+              </button>
+            </div>
+            <div className="flex justify-center items-center">
+              <button className="bg-[#7D9D9C] h-[10vh] w-[20vw] font-['consolas'] text-[30px] rounded-md hover:scale-[1.05]" onClick={() => onClick("teacher")}>
+                Teacher
+              </button>
+            </div>
+          </> )}
+        </div>
       </div>
     );
   } else {

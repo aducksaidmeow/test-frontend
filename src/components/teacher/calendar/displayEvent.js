79dfe71895ca623f16507c6e3627db18ced77754
aducksaidmeow <Duck7D6@gmail.com>
2022-11-28 11:31:58 +0700

    add tailwindcss

--- src/components/teacher/calendar/displayEvent.js
+++ src/components/teacher/calendar/displayEvent.js
@@ -1,45 +1,57 @@
 import React from "react";
 import { formatDate } from "@fullcalendar/react";
-import './displayEvent.css'
 
-export default function DisplayEvent({ open, setOpen, content, setContent }) {
+export default function DisplayEvent({ render, setRender, display, setDisplay}) {
 
-    return(
-        <div className="teacher-display-event-container">
-            <div className="teacher-display-event-close-button" onClick={() => setOpen(false)}>x</div>
-            <div className="teacher-display-event-title">{content.title}</div>
-            <div className="teacher-display-event-description">{content.description}</div>
-            <div className="teacher-display-event-group">{content.group}</div>
-            <div className="teacher-display-event-start-time">{formatDate(content.startTime, {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                hour12: true
-            })}</div>
-            <div className="teacher-display-event-end-time">{content.endTime === null ? formatDate(content.startTime, {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                hour12: true
-            }) : formatDate(content.endTime, {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                hour12: true
-            })}</div>
-            <div className="teacher-display-event-id">{content.id}</div>
+    const onClick = () => {
+        setRender({
+            loading: false,
+            calendar: true,
+            display: false,
+            addEvent: false,
+            addGroup: false,
+            removeEvent: false,
+        });
+    }
+
+    return (
+        <div className="h-[90vh] w-[90vw] flex justify-center items-center font-['consolas'] shadow-2xl overflow-y-auto scrollbar-hide bg-[#EEEEEE]">
+            <div className="h-[75vh] w-[50vw] bg-[#CCD1E4] rounded-lg relative flex justify-center items-center flex-col gap-[2.5vh]">
+                <button className="absolute top-0 right-0 bg-[#DC3535] h-[5vh] w-[2.5vw] rounded-lg" onClick={() => onClick()}>X</button>
+                <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{display.title}</div>
+                <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{display.description}</div>
+                <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{display.group}</div>
+                <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{formatDate(display.startTime, {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    weekday: "long",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: true
+                })}</div>
+                <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{display.endTime === null ? formatDate(display.startTime, {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    weekday: "long",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: true
+                }) : formatDate(display.endTime, {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    weekday: "long",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: true
+                })}</div>
+                <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{display.id}</div>
+            </div>
         </div>
     );
 }
--- src/components/teacher/calendar/teacherCalendar.js
+++ src/components/teacher/calendar/teacherCalendar.js
@@ -2,74 +2,49 @@ import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import googleCalendarPlugin from "@fullcalendar/google-calendar";
-import DisplayEvent from "./displayEvent";
 import axios from "axios";
-import "./teacherCalendar.css";
-import LoadingScreen from "./loadingScreen/loadingScreen";
 
-export default function TeacherCalendar() {
-  const [open, setOpen] = useState(false);
-  const [content, setContent] = useState({});
-
-  const [loading, setLoading] = useState(false);
+export default function TeacherCalendar({ render, setRender, display, setDisplay}) {
 
   const eventClick = (eventInfo) => {
     eventInfo.jsEvent.preventDefault();
-    setLoading(true);
-    console.log(eventInfo);
     const eid = eventInfo.event.url.split("=")[1];
     const userId = localStorage.getItem("userId");
-    const url =
-      process.env.NODE_ENV === "production"
-        ? `${process.env.REACT_APP_API_URL}/api/get-event`
-        : "/api/get-event";
-    axios
-      .post(url, { userId, eid })
-      .then((response) => {
-        console.log(response.data);
-        const eventContent = {
-          title: eventInfo.event.title,
-          description: response.data.data.description,
-          group: response.data.data.extendedProperties.shared.groupName,
-          startTime: eventInfo.event.start,
-          endTime: eventInfo.event.end,
-          id: response.data.data.id,
-        };
-        setContent(eventContent);
-        setLoading(false);
-        setOpen(true);
-        console.log(content);
+    const url = process.env.NODE_ENV === "production" ? `${process.env.REACT_APP_API_URL}/api/get-event` : "/api/get-event";
+    axios.post(url, { userId, eid }).then((response) => {
+      setDisplay({
+        title: eventInfo.event.title,
+        description: response.data.data.description,
+        group: response.data.data.extendedProperties.shared.groupName,
+        startTime: eventInfo.event.start,
+        endTime: eventInfo.event.end,
+        id: response.data.data.id,
+      });
+      setRender({
+        loading: false,
+        calendar: false,
+        display: true,
+        addEvent: false,
+        addGroup: false,
+        removeEvent: false,
       })
-      .catch((error) => console.log(error));
+    }).catch((error) => console.log(error));
   };
 
   return (
-    <div>
-      <div className="teacher-calendar-container">
-        {!loading && (
-          <FullCalendar
-            plugins={[dayGridPlugin, googleCalendarPlugin]}
-            initialView="dayGridMonth"
-            googleCalendarApiKey={process.env.REACT_APP_API_KEY}
-            events={{
-              googleCalendarId: localStorage.getItem("email"),
-            }}
-            contentHeight="auto"
-            eventDisplay="block"
-            eventColor="#87CEEB"
-            eventClick={(eventInfo) => eventClick(eventInfo)}
-          />
-        )}
-      </div>
-      {open && (
-        <DisplayEvent
-          open={open}
-          setOpen={setOpen}
-          content={content}
-          setContent={setContent}
-        />
-      )}
-      {loading && <LoadingScreen />}
+    <div className="h-[90vh] w-[90vw] font-['consolas'] shadow-2xl overflow-y-auto scrollbar-hide bg-[#EEEEEE]">
+      <FullCalendar
+        plugins={[dayGridPlugin, googleCalendarPlugin]}
+        initialView="dayGridMonth"
+        googleCalendarApiKey={process.env.REACT_APP_API_KEY}
+        events={{
+          googleCalendarId: localStorage.getItem("email"),
+        }}
+        contentHeight="auto"
+        eventDisplay="block"
+        eventColor="#066163"
+        eventClick={(eventInfo) => eventClick(eventInfo)}
+      />
     </div>
   );
 }

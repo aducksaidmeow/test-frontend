42de7817c66373a8fab7288f9b9b3a904cdff6e8
aducksaidmeow <Duck7D6@gmail.com>
2022-11-29 01:08:41 +0700

    redesign student-calendar page

--- src/components/student/studentMain.js
+++ src/components/student/studentMain.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import StudentCalendar from "./calendar/studentCalendar";
+import DisplayEvent from "./displayEvent/displayEvent";
+import LoadingScreen from "./loadingScreen/loadingScreen";
 
 export default function TeacherMain() {
+
+    const [render, setRender] = useState({
+        loading: false,
+        calendar: true,
+        display: false,
+    })
+
+    const [display, setDisplay] = useState({});
 
     const email = localStorage.getItem('email');
     const role = localStorage.getItem('role');
 
     if (email !== '' && email != null && role === 'student') {
         return (
-            <StudentCalendar />
+            <div className="h-screen bg-[#F0EBE3] flex justify-center items-center">
+                {render.calendar && 
+                    <StudentCalendar render={render} setRender={setRender} display={display} setDisplay={setDisplay}/>
+                }
+                {render.display && 
+                    <DisplayEvent render={render} setRender={setRender} display={display} setDisplay={setDisplay} />
+                }
+                {render.loading && 
+                    <LoadingScreen />
+                }
+            </div>
         )
     } else {
         return (

93407e321626e995f6ce8352c0d25f8b48404805
aducksaidmeow <Duck7D6@gmail.com>
2022-11-28 11:26:20 +0700

    add tailwindcss

--- src/components/student/calendar/displayEvent.js
+++ src/components/student/calendar/displayEvent.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { formatDate } from "@fullcalendar/react";
-import './displayEvent.css'
 
 export default function DisplayEvent({ open, setOpen, content, setContent }) {
-
-    return(
-        <div className="student-display-event-container">
-            <div className="student-display-event-close-button" onClick={() => setOpen(false)}>x</div>
-            <div className="student-display-event-title">{content.title}</div>
-            <div className="student-display-event-description">{content.description}</div>
-            <div className="student-display-event-group">{content.group}</div>
-            <div className="student-display-event-start-time">{formatDate(content.startTime, {
+    return (
+        <div className="h-[75vh] w-[50vw] bg-[#CCD1E4] rounded-lg relative flex justify-center items-center flex-col gap-[2.5vh]">
+            <button className="absolute top-0 right-0 bg-[#DC3535] h-[5vh] w-[2.5vw] rounded-lg" onClick={() => setOpen(false)}>X</button>
+            <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{content.title}</div>
+            <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{content.description}</div>
+            <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{content.group}</div>
+            <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{formatDate(content.startTime, {
                 year: "numeric",
                 month: "2-digit",
                 day: "2-digit",
@@ -20,7 +18,7 @@ export default function DisplayEvent({ open, setOpen, content, setContent }) {
                 second: "2-digit",
                 hour12: true
             })}</div>
-            <div className="student-display-event-end-time">{content.endTime === null ? formatDate(content.startTime, {
+            <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{content.endTime === null ? formatDate(content.startTime, {
                 year: "numeric",
                 month: "2-digit",
                 day: "2-digit",
@@ -39,7 +37,7 @@ export default function DisplayEvent({ open, setOpen, content, setContent }) {
                 second: "2-digit",
                 hour12: true
             })}</div>
-            <div className="student-display-event-id">{content.id}</div>
+            <div className="h-[7.5vh] w-[35vw] bg-[#F9F9F9] rounded-md flex justify-center items-center">{content.id}</div>
         </div>
     );
 }

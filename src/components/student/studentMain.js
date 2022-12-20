ea9faa93ab5eff20d20e4ba631afad1bdc078601
aducksaidmeow <Duck7D6@gmail.com>
2022-12-20 21:13:24 +0700

    add instruction and event-remove functionality

--- src/components/student/studentMain.js
+++ src/components/student/studentMain.js
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import RemoveEventButton from "./removeEvent/removeEventButton";
+import RemoveEventMenu from "./removeEvent/removeEventMenu";
+import InstructionButton from "./instruction/instructionButton";
+import InstructionMenu from "./instruction/instructionMenu";
 import StudentCalendar from "./calendar/studentCalendar";
 import DisplayEvent from "./displayEvent/displayEvent";
 import LoadingScreen from "./loadingScreen/loadingScreen";
@@ -9,6 +13,8 @@ export default function TeacherMain() {
         loading: false,
         calendar: true,
         display: false,
+        removeEvent: false,
+        instruction: false,
     })
 
     const [display, setDisplay] = useState({});
@@ -18,16 +24,28 @@ export default function TeacherMain() {
 
     if (email !== '' && email != null && role === 'student') {
         return (
-            <div className="h-screen bg-gradient-to-l from-indigo-200 via-red-200 to-yellow-100 flex justify-center items-center">
-                {render.calendar && 
-                    <StudentCalendar render={render} setRender={setRender} display={display} setDisplay={setDisplay}/>
-                }
-                {render.display && 
-                    <DisplayEvent render={render} setRender={setRender} display={display} setDisplay={setDisplay} />
-                }
-                {render.loading && 
-                    <LoadingScreen />
-                }
+            <div className="h-screen grid grid-cols-16 bg-gradient-to-l from-indigo-200 via-red-200 to-yellow-100">
+                <div className="flex flex-col items-center pt-[10vh] gap-[1vh]">
+                    <RemoveEventButton render={render} setRender={setRender} />
+                    <InstructionButton render={render} setRender={setRender} />
+                </div>
+                <div className="col-start-2 col-span-15 flex justify-center items-center">
+                    {render.calendar && 
+                        <StudentCalendar render={render} setRender={setRender} display={display} setDisplay={setDisplay}/>
+                    }
+                    {render.display && 
+                        <DisplayEvent render={render} setRender={setRender} display={display} setDisplay={setDisplay} />
+                    }
+                    {render.loading && 
+                        <LoadingScreen />
+                    }
+                    {render.removeEvent && 
+                        <RemoveEventMenu render={render} setRender={setRender} />
+                    }
+                    {render.instruction && 
+                        <InstructionMenu render={render} setRender={setRender} />
+                    }
+                </div> 
             </div>
         )
     } else {

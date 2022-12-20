44de460df52c845ec5ddb5df85fb33dbce76433c
aducksaidmeow <Duck7D6@gmail.com>
2022-12-20 21:12:19 +0700

    new file

---
+++ src/components/student/removeEvent/removeEventButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import removeEvent from "./remove-event.png"
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale-extreme.css';
+
+export default function RemoveEventButton({ render, setRender}) {
+
+    const onClick = () => {
+        const newRender = {...render};
+        for(const value in newRender) newRender[value] = false;
+        newRender.removeEvent = true;
+        setRender(newRender);
+    }
+
+    return (
+        <>
+            <Tippy content="Xóa bài tập" placement="right" animation="scale-extreme">
+                <button className="h-[11.5vh] w-[5.5vw] rounded-md bg-[#E97777] flex justify-center items-center hover:translate-x-[1.5vw]"
+                onClick={() => onClick()}>
+                    <img src={removeEvent} className="h-[9vh] w-[4vw]"/>
+                </button>
+            </Tippy>
+        </> 
+    );
+}

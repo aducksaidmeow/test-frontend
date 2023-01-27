f3e7705f3df4bf869644f033864c8cd13836bc65
aducksaidmeow <Duck7D6@gmail.com>
2023-01-27 16:30:56 +0700

    add group-adding functionality

--- src/components/teacher/teacherMain.js
+++ src/components/teacher/teacherMain.js
@@ -35,11 +35,11 @@ export default function TeacherMain() {
         return (
             <div className="h-screen grid grid-cols-16 bg-gradient-to-l from-indigo-200 via-red-200 to-yellow-100 overflow-y-auto overflow-x-auto scrollbar-hide">
                 <div className="flex flex-col items-center pt-[10vh] gap-[1vh]">
-                    {/*<AddGroupButton render={render} setRender={setRender} />*/}
+                    <AddGroupButton render={render} setRender={setRender} />
                     <AddEventButton render={render} setRender={setRender} />
                     {/*<RemoveEventButton render={render} setRender={setRender}/>*/}
                     <DisplayGroupButton render={render} setRender={setRender} />
-                    {/*<InstructionButton render={render} setRender={setRender} />*/}
+                    <InstructionButton render={render} setRender={setRender} />
                 </div>
                 <div className="col-start-2 col-span-15 flex justify-center items-center">
                     {render.calendar && 
@@ -51,9 +51,9 @@ export default function TeacherMain() {
                     {render.addEvent &&
                         <AddEventMenu render={render} setRender={setRender} />
                     }
-                    {/*{render.addGroup && 
+                    {render.addGroup && 
                         <AddGroupMenu render={render} setRender={setRender} />
-                    }*/}
+                    }
                     {/*{render.removeEvent &&
                         <RemoveEventMenu render={render} setRender={setRender} />
                     }*/}
@@ -63,9 +63,9 @@ export default function TeacherMain() {
                     {render.displayGroup && 
                         <DisplayGroupMenu render={render} setRender={setRender} />
                     }
-                    {/*{render.instruction && 
+                    {render.instruction && 
                         <InstructionMenu render={render} setRender={setRender} />
-                    }*/}
+                    }
                 </div>
             </div>
         );
